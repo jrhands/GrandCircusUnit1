961928bb9a3d9c41ac78dd8bdcaccb3f33b55e6b
JacobPersonal <jrhands@gmail.com>
2018-07-05 05:45:02 -0500

    Added error handling and comments.

--- Lab1Part2/Lab1Part2/Program.cs
+++ Lab1Part2/Lab1Part2/Program.cs
@@ -6,15 +6,35 @@ namespace Lab1Part2
     {
         static void Main(string[] args)
         {
-            DateTime firstDate, secondDate;
+            Boolean unverified = true;
+            DateTime firstDate = DateTime.Parse("1/1/2000");
+            DateTime secondDate = DateTime.Parse("1/1/2001");
             TimeSpan diff;
-            Console.WriteLine("Enter the first date.");
-            firstDate = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second date.");
-            secondDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Welcome to the date checker!");
+
+            //Looping and try/catch to handle invalid datetime inputs
+            while (unverified)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the first date.");
+                    firstDate = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the second date.");
+                    secondDate = DateTime.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid date. Try again.");
+                    continue;
+                }
+                unverified = false;
+            }
+
+            //Calculates the difference between the two dates and displays it
             diff = firstDate.Subtract(secondDate).Duration();
             Console.WriteLine("The difference between the two dates is:");
             Console.WriteLine(diff.Days + " Days, " + diff.Hours + " Hours, and " + diff.Minutes + " Minutes");
+            Console.WriteLine("Press enter to close...");
             Console.Read();
         }
     }
