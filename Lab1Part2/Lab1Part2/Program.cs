057e572e6540f31e68736f00a721fd266d801d5f
JacobPersonal <jrhands@gmail.com>
2018-07-05 04:53:16 -0500

    Added basic functionality of part 2, need to add comments and error handling

--- Lab1Part2/Lab1Part2/Program.cs
+++ Lab1Part2/Lab1Part2/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Lab1Part2
+﻿using System;
+
+namespace Lab1Part2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DateTime firstDate, secondDate;
+            TimeSpan diff;
+            Console.WriteLine("Enter the first date.");
+            firstDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second date.");
+            secondDate = DateTime.Parse(Console.ReadLine());
+            diff = firstDate.Subtract(secondDate).Duration();
+            Console.WriteLine("The difference between the two dates is:");
+            Console.WriteLine(diff.Days + " Days, " + diff.Hours + " Hours, and " + diff.Minutes + " Minutes");
+            Console.Read();
         }
     }
 }
